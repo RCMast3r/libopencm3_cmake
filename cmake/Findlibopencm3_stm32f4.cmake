964a448d23e284243167d609a15aee0b7266a965
Ben Hall <rcmast3r1@gmail.com>
2023-08-26 21:12:30 -0400

    made initial cmake for stm32f4 library

--- cmake/Findlibopencm3_stm32f4.cmake
+++ cmake/Findlibopencm3_stm32f4.cmake
@@ -1,30 +1,27 @@
 include(GNUInstallDirs)
-message(${PROJECT_BINARY_DIR})
 
 find_library(
     libopencm3_stm32f4_LIBRARY
-    NAMES libopencm3_stm32f4
-    HINTS ${PROJECT_BINARY_DIR}/
-    PATH_SUFFIXES lib/)
+    NAMES libopencm3_stm32f4.a
+    HINTS ${CMAKE_LIBRARY_PATH}
+    )
 
-find_path(libopencm3_INCLUDE_DIR
-  NAMES Pipeline.hpp
-  HINTS ${PROJECT_BINARY_DIR}/ ${CMAKE_INSTALL_INCLUDEDIR}
-  PATH_SUFFIXES include)
-
+find_path(libopencm3_stm32f4_INCLUDE_DIR
+  HINTS ${CMAKE_INCLUDE_PATH}/ 
+  NAMES libopencm3 libopencmsis
+  )
 include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(libopencm3_stm32f4 DEFAULT_MSG
+                                  libopencm3_stm32f4_LIBRARY
+                                  libopencm3_stm32f4_INCLUDE_DIR)
 
-find_package_handle_standard_args(libopencm3 DEFAULT_MSG
-                                  libopencm3_LIBRARY
-                                  libopencm3_INCLUDE_DIR)
+mark_as_advanced(libopencm3_stm32f4_LIBRARY libopencm3_stm32f4_INCLUDE_DIR)
 
-mark_as_advanced(libopencm3_LIBRARY libopencm3_INCLUDE_DIR)
-
-if(libopencm3_FOUND AND NOT TARGET libopencm3::libopencm3_stm32f4)
+if(libopencm3_stm32f4_FOUND AND NOT TARGET libopencm3::libopencm3_stm32f4)
   add_library(libopencm3::libopencm3_stm32f4 STATIC IMPORTED)
   set_target_properties(
     libopencm3::libopencm3_stm32f4
     PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES "${libopencm3_INCLUDE_DIR}"
-      IMPORTED_LOCATION ${libopencm3_LIBRARY})
+      INTERFACE_INCLUDE_DIRECTORIES "${libopencm3_stm32f4_INCLUDE_DIR}"
+      IMPORTED_LOCATION ${libopencm3_stm32f4_LIBRARY})
 endif()
--- default.nix
+++ default.nix
@@ -4,5 +4,6 @@ stdenv.mkDerivation rec {
   pname = "libopencm3_stm32f4";
   version = "0.1.0";
   src = ./.;
-  nativeBuildInputs = [ cmake gcc-arm-embedded libopencm3];
+  nativeBuildInputs = [ cmake gcc-arm-embedded ];
+  propagatedBuildInputs = [ libopencm3 ];
 }
