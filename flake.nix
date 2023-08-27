298cfcde868c399165aaa86768d0fc0b4c00afee
Ben Hall <rcmast3r1@gmail.com>
2023-08-26 20:14:08 -0400

    added in default package and added libopencm3_stm32f4 to overlay list itself

--- flake.nix
+++ flake.nix
@@ -4,17 +4,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     utils.url = "github:numtide/flake-utils";
-    libopencm3.url = "path:/home/ben/hytech/libopencm3";
+    libopencm3.url = "github:RCMast3r/libopencm3";
   };
   outputs = { self, nixpkgs, utils, libopencm3 }:
-    let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        overlays = [ libopencm3.overlays.default ];
-      };
-    in
     {
-      asdf = pkgs.callPackage ./default.nix { };
-    };
+      overlays.default = final: prev: {
+        libopencm3_stm32f4 = final.callPackage ./default.nix { };
+      };
+      packages.x86_64-linux =
+        let
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            overlays = [ self.overlays.default libopencm3.overlays.default ];
+          };
+        in
+        rec {
+          libopencm3_stm32f4 = pkgs.libopencm3_stm32f4;
+          default = libopencm3_stm32f4;
+        };
 
+    };
 }
