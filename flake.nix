5d4fca9d82a59eb6114608394241b78babe06d1d
Ben Hall <rcmast3r1@gmail.com>
2023-08-29 15:00:22 -0400

    actually propagating overlays

--- flake.nix
+++ flake.nix
@@ -7,15 +7,22 @@
     libopencm3.url = "github:RCMast3r/libopencm3";
   };
   outputs = { self, nixpkgs, utils, libopencm3 }:
-    {
-      overlays.default = final: prev: {
+    let
+      libopencm3_stm32f4_overlay = final: prev: {
         libopencm3_stm32f4 = final.callPackage ./default.nix { };
       };
+
+      my_overlays = [ libopencm3.overlays.default libopencm3_stm32f4_overlay ];
+    in
+    {
+
+
+      overlays.default = nixpkgs.lib.composeManyExtensions my_overlays;
       packages.x86_64-linux =
         let
           pkgs = import nixpkgs {
             system = "x86_64-linux";
-            overlays = [ self.overlays.default libopencm3.overlays.default ];
+            overlays = [ self.overlays.default ];
           };
         in
         rec {
