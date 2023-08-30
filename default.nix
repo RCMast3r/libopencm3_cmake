6c1e246f1a79d4adb3b4b2a620365fe1c8776d0d
Ben Hall <rcmast3r1@gmail.com>
2023-08-30 16:23:47 -0400

    pushing up better libopencm3_cmake

--- default.nix
+++ default.nix
@@ -1,9 +1,13 @@
-{ stdenv, cmake, gcc-arm-embedded, libopencm3 }:
+{ stdenv, gnumake, cmake, gcc-arm-embedded, libopencm3 }:
 
 stdenv.mkDerivation rec {
   pname = "libopencm3_stm32f4";
   version = "0.1.0";
   src = ./.;
-  nativeBuildInputs = [ cmake gcc-arm-embedded ];
+  nativeBuildInputs = [ gnumake cmake gcc-arm-embedded ];
+  # dontPatch = true;
+  # dontFixup = true;
+  # dontStrip = true;
+  # dontPatchELF = true;
   propagatedBuildInputs = [ libopencm3 ];
 }
